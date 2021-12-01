e5c48d84889ab56237eff3164876b385a282096f
Omri Tamam <omri.tamam@cs.huji.ac.il>
2021-12-01 13:41:01 +0200

    start

---
+++ src/ascii_art/Driver.java
@@ -0,0 +1,7 @@
+package ascii_art;
+
+public class Driver {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ src/ascii_art/img_to_char/BrightnessImgCharMatcher.java
@@ -0,0 +1,12 @@
+package ascii_art.img_to_char;
+
+public class BrightnessImgCharMatcher {
+    public char[][] chooseChars(int numCharsInRow, Character[] charSet){
+        char[][] result = new char[numCharsInRow][];
+        for(Character c : charSet){
+        }
+        return result;
+    }
+
+
+}
